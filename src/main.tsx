f2489de5d16ab9c8a7a15d6f12b2dcea15455070
Pradeep Suthar <pradeexsu@gmail.com>
2024-04-20 18:08:33 +0530

    ping mechanism added for free pod spin up

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './modules/Auth';
 import { NotificationProvider } from './modules/Notification';
+import { PingProvider } from './modules/Ping';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <NotificationProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </NotificationProvider>
-    </BrowserRouter>
+    <PingProvider>
+      <BrowserRouter>
+        <NotificationProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </NotificationProvider>
+      </BrowserRouter>
+    </PingProvider>
   </React.StrictMode>
 );
---
+++ src/modules/Ping/ping.service.ts
@@ -0,0 +1,27 @@
+import { axiosAuthInstance } from '../Axios/axios.instance';
+import { ApiResponse } from '../Axios/typings';
+import { AxiosResponse } from 'axios';
+import { NotificationService } from '../Notification';
+
+class PingService extends NotificationService {
+  constructor() {
+    super();
+  }
+  ping = async () => {
+    try {
+      const res = (await axiosAuthInstance.get(
+        'ping',
+      )) as AxiosResponse<ApiResponse<unknown>>;
+
+      if (res.status <= 299 && res.status >= 200) {
+        // this.pushInfoNotification('Service Connected!')
+        return true;
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
+  }
+}
+
+export default new PingService();
